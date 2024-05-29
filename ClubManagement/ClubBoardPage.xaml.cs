34dc43493e44f5cde51cad72015dc0ad79e94dcd
zx462 <rladuwns1917@naver.com>
2024-05-30 01:10:44 +0900

    세부사항 페이지, 게시글 페이지 구현

---
+++ ClubManagement/ClubBoardPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClubManagement
+{
+    public class Post
+    {
+        public int Index { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime DatePosted { get; set; }
+        public string Content { get; set; }
+    }
+    public class ViewModel
+    {
+        public ObservableCollection<Post> Posts { get; set; }
+
+        public ViewModel()
+        {
+            // 예시 데이터 생성
+            Posts = new ObservableCollection<Post>
+        {
+            new Post { Index = 1, Title = "첫 번째 게시물", Author = "사용자1", Content = "안녕하세요, 첫 번째 게시물입니다.", DatePosted = DateTime.Now.AddDays(-3) },
+            new Post { Index = 2, Title = "두 번째 게시물", Author = "사용자2", Content = "안녕하세요, 두 번째 게시물입니다.", DatePosted = DateTime.Now.AddDays(-2) },
+            new Post { Index = 3, Title = "세 번째 게시물", Author = "사용자3", Content = "안녕하세요, 세 번째 게시물입니다.", DatePosted = DateTime.Now.AddDays(-1) }
+        };
+        }
+    }
+    /// <summary>
+    /// ClubBoardPage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ClubBoardPage : Window
+    {
+        public ClubBoardPage()
+        {
+            InitializeComponent();
+            DataContext = new ViewModel();
+        }
+    }
+}
--- ClubManagement/ClubDetailPage.xaml.cs
+++ ClubManagement/ClubDetailPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,11 +20,65 @@ namespace ClubManagement
     /// <summary>
     /// ClubDetailPage.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class ClubDetailPage : Window
+    public partial class ClubDetailPage : Window, INotifyPropertyChanged
     {
+        private DateTime _displayedMonth;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<DateModel> Dates { get; set; }
+
+        public DateTime DisplayedMonth
+        {
+            get => _displayedMonth;
+            set
+            {
+                _displayedMonth = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("DisplayedMonth"));
+            }
+        }
+
+
         public ClubDetailPage()
         {
             InitializeComponent();
+            DataContext = this;
+            DisplayedMonth = DateTime.Today;
+            Dates = new ObservableCollection<DateModel>();
+            PopulateDates();
+
+        }
+        private void PopulateDates()
+        {
+            Dates.Clear();
+            int daysInMonth = DateTime.DaysInMonth(DisplayedMonth.Year, DisplayedMonth.Month);
+            for (int day = 1; day <= daysInMonth; day++)
+            {
+                Dates.Add(new DateModel { Day = day });
+            }
+        }
+
+        private void PreviousMonth_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayedMonth = DisplayedMonth.AddMonths(-1);
+            PopulateDates();
+        }
+
+        private void NextMonth_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayedMonth = DisplayedMonth.AddMonths(1);
+            PopulateDates();
         }
     }
+
+    public class DateModel
+    {
+        public int Day { get; set; }
+        // Add other properties like year, month, or any other data you want to display
+    }
+
+
 }
+   
+
+
